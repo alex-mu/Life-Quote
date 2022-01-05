b0dfbe1fd5d2998513862de76d8740505eaa3f81
Alexander <alexndegwa46@gmail.com>
2022-01-05 13:37:18 +0300

    generated quote details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsComponent } from './forms/forms.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormsComponent,
     QuotesComponent,
+    QuoteDetailsComponent,
   ],
   imports: [
     BrowserModule,
