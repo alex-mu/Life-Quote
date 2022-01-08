54a2ad4955dc85425addf0bcae94b6616428c9b1
Alexander <alexndegwa46@gmail.com>
2022-01-08 17:18:33 +0300

    corrected text in quote

--- src/app/goal-form/goal-form.component.ts
+++ src/app/goal-form/goal-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Goal } from '../goal';
 
 @Component({
   selector: 'app-goal-form',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./goal-form.component.css']
 })
 export class GoalFormComponent implements OnInit {
+  newGoal = new Goal(0,"","",new Date());
+@Output() addGoal = new EventEmitter<Goal>();
 
+  submitGoal(){
+this.addGoal.emit(this.newGoal);
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -23,6 +23,12 @@ export class GoalComponent  {
       this.goals.splice(index,1);
     }
   }
+  addNewGoal(goal: Goal){
+    let goalLength = this.goals.length;
+    goal.id = goalLength+1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
   deleteGoal(isComplete: boolean, index: number){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
@@ -31,6 +37,7 @@ export class GoalComponent  {
       }
     }
   }
+
 }
 
 
